1b8e1f7f33acf648a15e743671630265c93f8662
Racheal438 <rachiequeen5@gmail.com>
2022-06-07 16:43:50 +0100

    completed the tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 // complete the function
 function prime (num) {
-  // code goes here
+  if (num === 1) {
+    return false;
+  }
+  else if(num === 2) {
+    return true;
+  }
+  else {
+    for(var x = 2; x < num; x++) {
+      if(num % x === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 function solution (arg) {
